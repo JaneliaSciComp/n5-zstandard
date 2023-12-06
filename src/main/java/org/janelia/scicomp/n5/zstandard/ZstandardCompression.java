391f6fd86720f0ee1c1897bf5ab0bdd3003d7733
Mark Kittisopikul <kittisopikulm@janelia.hhmi.org>
2023-12-05 19:12:38 -0500

    Enhance JavaDoc. Use windowLog for input as well.

--- src/main/java/org/janelia/scicomp/n5/zstandard/ZstandardCompression.java
+++ src/main/java/org/janelia/scicomp/n5/zstandard/ZstandardCompression.java
@@ -18,11 +18,10 @@ import org.janelia.saalfeldlab.n5.DefaultBlockWriter;
 /**
  * Zstandard compression for N5
  * 
- * Implementation wrapper around zstd-jni
- * https://github.com/luben/zstd-jni
+ * Implementation wrapper around <a href="https://github.com/luben/zstd-jni">zstd-jni</a>.
  * 
- * See the Zstandard manual for details on parameters.
- * https://facebook.github.io/zstd/zstd_manual.html
+ * See the <a href="https://facebook.github.io/zstd/zstd_manual.html">Zstandard manual</a> for details on parameters.
+ * 
  * 
  * @author mkitti
  *
@@ -30,6 +29,11 @@ import org.janelia.saalfeldlab.n5.DefaultBlockWriter;
 @CompressionType("zstd")
 public class ZstandardCompression implements DefaultBlockReader, DefaultBlockWriter, Compression {
 	private static final long serialVersionUID = 5811954066059985371L;
+	
+	/*
+	 * Default compression level from zstd.h
+	 */
+	public static final int ZSTD_CLEVEL_DEFAULT = 3;
 	
 	/*
 	 * Compression level
@@ -40,7 +44,7 @@ public class ZstandardCompression implements DefaultBlockReader, DefaultBlockWri
 	 * Default: 3
 	 */
 	@CompressionParameter
-	private int level = 3;
+	private int level = ZSTD_CLEVEL_DEFAULT;
 	
 	/*
 	 * Number of Worker Threads to spawn
@@ -137,18 +141,11 @@ public class ZstandardCompression implements DefaultBlockReader, DefaultBlockWri
 	 * Configure how buffers are recycled
 	 */
 	private BufferPool bufferPool = NoPool.INSTANCE;
+
 	
-	/*
-	 * Default compression level from zstd.h
-	 */
-	public static final int ZSTD_CLEVEL_DEFAULT = 3;
-	
-	/*
-	 * Sets the following default parameters
-	 * level: 3
-	 * nbWorkers: 0
-	 * windowLog: 0
-	 * 
+	/**
+	 * Create ZstandardCompression with level equal to the constant ZSTD_CLEVEL_DEFAULT (value: {@value ZstandardCompression#ZSTD_CLEVEL_DEFAULT})
+	 *
 	 * Note that this uses a default compression level of 3 matching the C library.
 	 * Some libraries such as numcodecs use a default compression level of 1.
 	 */
@@ -159,128 +156,275 @@ public class ZstandardCompression implements DefaultBlockReader, DefaultBlockWri
 	}
 
 	/**
-	 * @param level -    Compression level (default: 3)
+	 * Create ZstandardCompression with the specified compression level.
+	 * 
+	 * @param level The standard compression levels are normally between 1 to 22. Negative compression levels offer greater speed.
+	 *              The default value is  {@value ZstandardCompression#ZSTD_CLEVEL_DEFAULT}.
 	 */
 	public ZstandardCompression(int level) {
 		this.level = level;
 	}
 	
+	/*
+	 * Get the compression level
+	 */
 	public int getLevel() {
 		return level;
 	}
 
+	/**
+	 * Set the compression level
+	 * 
+	 * The standard compression levels are normally between 1 to 22. Negative compression levels offer greater speed.
+	 */
 	public void setLevel(int level) {
 		this.level = level;
 	}
 
+	/**
+	 * Get the Number of Worker Threads to spawn
+	 * 
+	 * Default: 0 (do not spawn any workers)
+	 */
 	public int getNbWorkers() {
 		return nbWorkers;
 	}
-
+	
+	/**
+	 * Set the Number of Worker Threads to spawn
+	 * 
+	 * Default: 0 (do not spawn any workers)
+	 */
 	public void setNbWorkers(int nbWorkers) {
 		this.nbWorkers = nbWorkers;
 	}
 
+	/**
+	 * Get the maximum allowed back-reference distance, expressed as a power of 2
+	 * 
+	 * Default: 0	
+	 */
 	public int getWindowLog() {
 		return windowLog;
 	}
 
+	/**
+	 * Set the maximum allowed back-reference distance, expressed as a power of 2
+	 * 
+	 * Default: 0	
+	 */
 	public void setWindowLog(int windowLog) {
 		this.windowLog = windowLog;
 	}
 	
+	/**
+	 * Set the size of the initial probe table, as a power of 2
+	 * 
+	 * Default: 0
+	 */
 	public int getHashLog() {
 		return hashLog;
 	}
 
+	/**
+	 * Get the size of the initial probe table, as a power of 2
+	 * 
+	 * Default: 0
+	 */
 	public void setHashLog(int hashLog) {
 		this.hashLog = hashLog;
 	}
 
+	/**
+	 * Get the size of the multi-probe search table, as a power of 2
+	 * 
+	 * Default: 0
+	 */
 	public int getChainLog() {
 		return chainLog;
 	}
 
+	/**
+	 * Set the size of the multi-probe search table, as a power of 2
+	 * 
+	 * Default: 0
+	 */
 	public void setChainLog(int chainLog) {
 		this.chainLog = chainLog;
 	}
 
+	/**
+	 * Get the number of search attempts, as a power of 2
+	 * 
+	 * Default: 0
+	 */
 	public int getSearchLog() {
 		return searchLog;
 	}
 
+	/**
+	 * Set the number of search attempts, as a power of 2
+	 * 
+	 * Default: 0
+	 */
 	public void setSearchLog(int searchLog) {
 		this.searchLog = searchLog;
 	}
 
+	/**
+	 * Get the minimum size of searched matches
+	 * 
+	 * Default: 0
+	 */
 	public int getMinMatch() {
 		return minMatch;
 	}
 
+	/**
+	 * Set the minimum size of searched matches
+	 * 
+	 * Default: 0
+	 */
 	public void setMinMatch(int minMatch) {
 		this.minMatch = minMatch;
 	}
 
+	/**
+	 * Impact of this field depends on strategy
+	 * 
+	 * Default: 0
+	 */
 	public int getTargetLength() {
 		return targetLength;
 	}
 
+	/**
+	 * Impact of setting this field depends on strategy
+	 * 
+	 * Default: 0
+	 */
 	public void setTargetLength(int targetLength) {
 		this.targetLength = targetLength;
 	}
 
+	/**
+	 * See ZSTD_strategy enum definition
+	 * 
+	 * Default: 0
+	 */
 	public int getStrategy() {
 		return strategy;
 	}
 
+	/**
+	 * See ZSTD_strategy enum definition
+	 * 
+	 * Default: 0
+	 */
 	public void setStrategy(int strategy) {
 		this.strategy = strategy;
 	}
 
+	/**
+	 * Get the size of a compression job. This value is enforced only when {@code nbWorkers >= 1}
+	 * 
+	 * Default: 0
+	 */
 	public int getJobSize() {
 		return jobSize;
 	}
 
+	/**
+	 * Set the size of a compression job. This value is enforced only when {@code nbWorkers >= 1}
+	 * 
+	 * Default: 0
+	 */
 	public void setJobSize(int jobSize) {
 		this.jobSize = jobSize;
 	}
 
+	/**
+	 * Get the overlap size, as a fraction of window size
+	 * 
+	 * Default: 0
+	 */
 	public int getOverlapLog() {
 		return overlapLog;
 	}
 
+	/**
+	 * Set the overlap size, as a fraction of window size
+	 * 
+	 * Default: 0
+	 */
 	public void setOverlapLog(int overlapLog) {
 		this.overlapLog = overlapLog;
 	}
 
+	/**
+	 *  Check if checksums are used for the compressed stream
+	 * 
+	 * Default: false
+	 */	
 	public boolean isUseChecksums() {
 		return useChecksums;
 	}
 
+	/**
+	 *  Enable or disable checksums for the compressed stream
+	 * 
+	 * Default: false (disabled)
+	 */	
 	public void setUseChecksums(boolean useChecksums) {
 		this.useChecksums = useChecksums;
 	}
 
+	/**
+	 * Check if closing the frame on flush is enabled..
+	 * 
+	 * Default: false
+	 */
 	public boolean isSetCloseFrameOnFlush() {
 		return setCloseFrameOnFlush;
 	}
 
+	/**
+	 * Enable or disable closing the frame on flush.
+	 * 
+	 * Default: false
+	 */
 	public void setSetCloseFrameOnFlush(boolean setCloseFrameOnFlush) {
 		this.setCloseFrameOnFlush = setCloseFrameOnFlush;
 	}
 
+	/**
+	 * Get the dictionary for compression as a byte array
+	 * 
+	 * Default: null (no dictionary)
+	 */
 	public byte[] getDict() {
 		return dict;
 	}
 
+	/**
+	 * Set the dictionary for compression as a byte array
+	 * 
+	 * Default: null (no dictionary)
+	 */
 	public void setDict(byte[] dict) {
 		this.dict = dict;
 	}
 	
+	/**
+	 * Get how buffers are recycled
+	 */
 	public BufferPool getBufferPool() {
 		return bufferPool;
 	}
 
+	/**
+	 * Configure how buffers are recycled
+	 */
 	public void setBufferPool(BufferPool bufferPool) {
 		this.bufferPool = bufferPool;
 	}
@@ -328,7 +472,16 @@ public class ZstandardCompression implements DefaultBlockReader, DefaultBlockWri
 
 	@Override
 	public InputStream getInputStream(InputStream in) throws IOException {
-		return new ZstdInputStream(in, bufferPool);
+		ZstdInputStream zstdIn =  new ZstdInputStream(in, bufferPool);
+		
+		//is windowLog the same as windowLogMax?
+		zstdIn.setLongMax(windowLog);
+		
+		if (dict != null) {
+			zstdIn.setDict(dict);
+		}
+		
+		return zstdIn;
 	}
 
 }
